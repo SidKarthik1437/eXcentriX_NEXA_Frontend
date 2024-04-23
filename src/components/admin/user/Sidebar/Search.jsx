edfbc9395a76fcfc4274af8efe241b5554a6d106
Sid Karthik <sa268592@gmail.com>
2024-04-23 12:33:40 +0530

    bug fixes

--- src/components/admin/user/Sidebar/Search.jsx
+++ src/components/admin/user/Sidebar/Search.jsx
@@ -1,17 +1,5 @@
-"use client";
-
 import * as React from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "@/components/ui/command";
 import { Input } from "@/components/ui/input";
 
 export default function Search({ users, setUsers }) {
