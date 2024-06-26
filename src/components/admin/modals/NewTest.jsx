40b476e2f632a06e043549f23ce3d28c72cbd10d
BhuvanSA <bhuvansaraj2003@gmail.com>
2024-06-26 15:45:01 +0530

    All fields added except for start and end dates

--- src/components/admin/modals/NewTest.jsx
+++ src/components/admin/modals/NewTest.jsx
@@ -1,10 +1,35 @@
-import axios from "axios";
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../../context/UserContext";
 import { DataContext } from "../../../context/DataContext";
 import { examServices } from "../../../api/services";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 function NewTest({ departments, subjects, setTestOpen }) {
+  const semesters = ["1", "2", "3", "4", "5", "6", "7", "8"];
   const [newData, setNewData] = useState({});
   const [durationParts, setDurationParts] = useState([]);
   const { tests, setTests } = useContext(DataContext);
@@ -54,226 +79,213 @@ function NewTest({ departments, subjects, setTestOpen }) {
     setTestOpen(false);
   };
 
+  const formSchema = z.object({});
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      subject: "",
+      duration: "",
+      total_marks: "",
+      passing_marks: "",
+    },
+  });
+
   return (
-    <div className="absolute flex flex-col w-5/6 h-auto border border-purple-300 bg-white rounded p-4 space-y-4 z-10 drop-shadow-2xl">
-      <div className="flex font-semibold tracking-wide text-xl justify-between items-center">
-        <span>New Test Configuration</span>
-        <button className="text-4xl p-0" onClick={() => setTestOpen(false)}>
-          &times;
-        </button>
-      </div>
-      <hr />
-      <div className="grid grid-cols-2 gap-4">
-        <div className="flex flex-col gap-2 justify-start">
-          <div className="flex items-center">
-            <span className="font-semibold mr-2 w-36">Subject: </span>
-            <select
-              className="rounded p-2 flex-grow border-purple-200 shadow shadow-purple-200"
-              value={newData.subject?.id}
-              onChange={(e) =>
-                setNewData({
-                  ...newData,
-                  subject: e.target.value,
-                })
-              }
-            >
-              <option class="dropdown-item" disabled selected value="undefined">
-                Select an Option
-              </option>
-              {subjects.map((subject, subIndex) => (
-                <option key={subject?.id} value={subject?.id}>
-                  {subject?.name}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className="flex items-center">
-            <span className="font-semibold mr-2 w-36">Exam Department:</span>
-            <select
-              //   value={exam?.department?.name}
-              onChange={(e) => {
-                setNewData({
-                  ...newData,
-                  department: e.target.value,
-                });
-              }}
-              className="rounded p-2 flex-grow border-purple-200 shadow shadow-purple-200"
-            >
-              <option class="dropdown-item" disabled selected value="undefined">
-                Select an Option
-              </option>
-              {departments.map((dept) => (
-                <option key={dept?.id} value={dept?.id} id={dept?.id}>
-                  {dept?.name}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-2 justify-start">
-          <div className="flex items-center">
-            <span className="font-semibold mr-2 w-36">Start Time:</span>
-            <input
-              type="datetime-local"
-              //   value={exam?.start_time.toString().slice(0, 16)}
-              onChange={(e) =>
-                setNewData({
-                  ...newData,
-                  start_time: e.target.value,
-                })
-              }
-              className="rounded p-2 flex-grow border-purple-200 shadow shadow-purple-200"
+    <Card className=" w-1/2 card-container relative rounded-xl border-purple-300 bg-white space-y-4 z-10">
+      <Button
+        className="absolute top-0 right-0 rounded-bl-2xl text-2xl rounded-tr-xl"
+        onClick={() => setTestOpen(false)}
+      >
+        &times;
+      </Button>
+      <CardHeader className="flex justify-center items-center">
+        <CardTitle className="">New Test Configuration</CardTitle>
+      </CardHeader>
+      <CardContent className="m-0">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSave)} className=" space-y-4">
+            <FormField
+              control={form.control}
+              name="department"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Subject :</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select the subject department" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {subjects.map((subject) => (
+                        <SelectItem value={subject.name} key={subject.id}>
+                          {subject.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="flex items-center">
-            <span className="font-semibold mr-2 w-36">End Time:</span>
-            <input
-              type="datetime-local"
-              //   value={exam?.end_time.toString().slice(0, 16)}
-              onChange={(e) =>
-                setNewData({
-                  ...newData,
-                  end_time: e.target.value,
-                })
-              }
-              className="rounded p-2 flex-grow border-purple-200 shadow shadow-purple-200"
+            <FormField
+              control={form.control}
+              name="department"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Subject Department:</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select the subject department" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {departments.map((department) => (
+                        <SelectItem value={department.name} key={department.id}>
+                          {department.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="flex items-center">
-            <span className="font-semibold mr-2 w-36">Duration:</span>
-            <div className="flex flex-grow gap-x-2 ">
-              {["hours", "minutes", "seconds"].map((part, index) => (
-                <div key={part} className="flex flex-col items-center">
-                  <input
-                    type="number"
-                    // value={durationParts[index]}
-                    onChange={(e) => handlePartChange(e, index)}
-                    className="rounded p-2 w-20 border-purple-200 shadow shadow-purple-200"
-                  />
-                  {part}
-                </div>
-              ))}
+            <FormField
+              control={form.control}
+              name="department"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Semester :</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select semester" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {semesters.map((semester) => (
+                        <SelectItem value={semester} key={semester.id}>
+                          {semester}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Total Questions :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="1" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Total Marks :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="100" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Passing Marks :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="40" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Marks Per Question :</FormLabel>
+                  <FormControl>
+                    <Input placeholder="4" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>Negative Marks</FormLabel>
+                  <FormControl>
+                    <Input placeholder="1" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* <FormField
+              control={form.control}
+              name="semester"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Semester</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select the subject semester" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {semesters.map((semester) => (
+                        <SelectItem value={semester} key={semester}>
+                          {semester}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            /> */}
+            <div className="flex justify-end">
+              <Button type="submit">Save</Button>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-3 gap-4">
-        {/* First Row */}
-        <div className="flex flex-col">
-          <label htmlFor="semester" className="font-semibold">
-            Semester:
-          </label>
-          <input
-            id="semester"
-            type="number"
-            onChange={(e) => {
-              setNewData((old) => ({
-                ...old,
-                semester: parseInt(e.target.value),
-              }));
-            }}
-            className="rounded p-2 border-purple-200 shadow shadow-purple-200"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="passingMarks" className="font-semibold">
-            Passing Marks:
-          </label>
-          <input
-            id="passingMarks"
-            type="number"
-            onChange={(e) =>
-              setNewData({
-                ...newData,
-                passingMarks: parseInt(e.target.value),
-              })
-            }
-            className="rounded p-2 border-purple-200 shadow shadow-purple-200"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="negativeMarks" className="font-semibold">
-            Negative Marks:
-          </label>
-          <input
-            id="negativeMarks"
-            type="number"
-            onChange={(e) =>
-              setNewData({
-                ...newData,
-                negativeMarks: parseInt(e.target.value),
-              })
-            }
-            className="rounded p-2 border-purple-200 shadow shadow-purple-200"
-          />
-        </div>
-
-        {/* Second Row */}
-        <div className="flex flex-col">
-          <label htmlFor="marksPerQuestion" className="font-semibold">
-            Marks Per Question:
-          </label>
-          <input
-            id="marksPerQuestion"
-            type="number"
-            onChange={(e) =>
-              setNewData({
-                ...newData,
-                marksPerQuestion: parseInt(e.target.value),
-              })
-            }
-            className="rounded p-2 border-purple-200 shadow shadow-purple-200"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="totalMarks" className="font-semibold">
-            Total Marks:
-          </label>
-          <input
-            id="totalMarks"
-            type="number"
-            onChange={(e) =>
-              setNewData({
-                ...newData,
-                totalMarks: parseInt(e.target.value),
-              })
-            }
-            className="rounded p-2 border-purple-200 shadow shadow-purple-200"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="totalQuestions" className="font-semibold">
-            Total Questions:
-          </label>
-          <input
-            id="totalQuestions"
-            type="number"
-            onChange={(e) =>
-              setNewData({
-                ...newData,
-                totalQuestions: parseInt(e.target.value),
-              })
-            }
-            className="rounded p-2 border-purple-200 shadow shadow-purple-200"
-          />
-        </div>
-      </div>
-
-      <div className="flex w-full items-center justify-between pt-10">
-        <button className="bg-red-500 text-white p-1 px-2 text-lg tracking-wider rounded">
-          Reset
-        </button>
-        <button
-          onClick={(e) => handleSave(e)}
-          className="bg-green-500 text-white p-1 px-2 text-lg tracking-wider rounded"
-        >
-          Save
-        </button>
-      </div>
-    </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
 
